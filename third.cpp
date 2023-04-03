36e11a1ba3629286d8d326e3131f83f876795452
husain zoopdiwala <husainabps@gmail.com>
2023-04-03 22:32:04 +0530

    this is first commit

---
+++ third.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    fflush(stdin);
+    cout<<s;
+    string t;
+    getline(cin,t,'\n');
+    cout<<"\n";
+    cout<<t;
+}
